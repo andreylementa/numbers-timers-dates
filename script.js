818047a84b0e02f7924fa517734fc92fc2330afc
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-02 17:05:36 +0300

    Оператор остаток

--- script.js
+++ script.js
@@ -75,3 +75,23 @@
 // console.log(Math.PI);
 // console.log(Math.PI.toFixed(2));
 // console.log(+(1.34567).toFixed(2));
+
+//////////////////////////////////////////////////
+// Оператор Остаток
+
+// console.log(7 % 3); // 7 = 3 * 2 + 1
+// console.log(7 % 4); // 7 = 4 * 1 + 3
+
+// console.log(8 % 2);
+// console.log(108 % 2);
+// console.log(8564 % 2);
+
+// console.log(9 % 2);
+// console.log(89 % 2);
+// console.log(881 % 2);
+
+// const isNumberOdd = x => x % 2 !== 0;
+// console.log(isNumberOdd(5));
+// console.log(isNumberOdd(7));
+// console.log(isNumberOdd(6));
+// console.log(isNumberOdd(10));
