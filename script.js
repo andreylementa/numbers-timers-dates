a092418d85a75e1c5a412416d1a23c66335beca8
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-03 12:45:39 +0300

    operation with dates

--- script.js
+++ script.js
@@ -154,3 +154,17 @@
 
 // futureDate.setFullYear(2223);
 // console.log(futureDate);
+
+//////////////////////////////////////////////////
+// Операции С Датами
+
+// const futureDate = new Date(2222, 1, 13, 11, 28, 59);
+// console.log(Number(futureDate));
+// console.log(+futureDate);
+// console.log(-futureDate);
+
+// const getDaysBetween2Dates = (date1, date2) =>
+//   Math.abs((date2 - date1) / (1000 * 60 * 60 * 24));
+
+// const days = getDaysBetween2Dates(new Date(2021, 9, 12), new Date(2021, 9, 1));
+// console.log(days);
