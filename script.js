13eff21cf13e109146ecaa5bfd06d6af14536127
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-03 14:49:27 +0300

    Intl numbers

--- script.js
+++ script.js
@@ -168,3 +168,20 @@
 
 // const days = getDaysBetween2Dates(new Date(2021, 9, 12), new Date(2021, 9, 1));
 // console.log(days);
+
+//////////////////////////////////////////////////
+// Интернационализация Чисел
+
+// const a = 325458478.21;
+
+// const options = {
+//   style: 'currency',
+//   unit: 'celsius',
+//   currency: 'EUR',
+// };
+
+// console.log('US', new Intl.NumberFormat('en-US', options).format(a));
+// console.log('Ukraine', new Intl.NumberFormat('uk-UA', options).format(a));
+// console.log('Russia', new Intl.NumberFormat('ru-RU', options).format(a));
+// console.log('Germany', new Intl.NumberFormat('de-DE', options).format(a));
+// console.log('Syria', new Intl.NumberFormat('ar-SY', options).format(a));
